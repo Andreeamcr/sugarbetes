f96bec60c6b98c7a3888a33d08fcd72826be42f1
Andreea Mocrii <mocriiandreea@yahoo.com>
2022-04-28 23:15:03 +0300

    modal progress HUD added

--- Project/lib/screens/sign_up_page.dart
+++ Project/lib/screens/sign_up_page.dart
@@ -7,7 +7,7 @@ import '../utils/background_design.dart';
 import 'package:sugarbetes/components/custom_circle_avatar.dart';
 import 'package:sugarbetes/components/form_field.dart';
 import 'package:sugarbetes/components/bottom_button.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class SignUpPage extends StatefulWidget {
   static String id = 'signUp';
@@ -38,6 +38,7 @@ class _SignUpPageState extends State<SignUpPage> {
     String? password;
     String? confirmPass;
     String kConfirmLabel="Confirmare parolă";
+    bool showSpinner = false;
     return Stack(children: [
       BackgroundColorWidget(),
       Scaffold(
@@ -54,104 +55,114 @@ class _SignUpPageState extends State<SignUpPage> {
         ),
         extendBodyBehindAppBar: true,
         backgroundColor: Colors.transparent,
-        body: Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: height * 0.07),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      MyCircleAvatar(
-                        imageUrl: 'assets/images/BloodSugar.jpg',
-                      ),
-                      SizedBox(
-                        height: height * 0.03,
-                      ),
-                      Text(
-                        'Creare cont',
-                        style: kWelcomeText,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              flex: 1,
-            ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20.0),
-                      topRight: Radius.circular(20.0)),
-                  color: kFullGreen,
-                ),
+        body: ModalProgressHUD(
+          inAsyncCall: showSpinner,
+          child: Column(
+            children: [
+              Expanded(
                 child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      SizedBox(
-                        height: height * 0.03,
-                      ),
-                      MyFormField(
-                          inputLabel: 'Introduceți numele',
-                          icon: Icon(Icons.people),
-                          obscure: false,
-                          suggestions: true,
-                          onChange: (value) {
-                            lastName = value;
-                          }),
-                      MyFormField(
-                          inputLabel: 'Introduceți prenumele',
-                          icon: Icon(Icons.people),
-                          obscure: false,
-                          suggestions: true,
-                          onChange: (value) {
-                            firstName = value;
-                          }),
-                      MyFormField(
-                          inputLabel: 'Introduceți adresa de email',
-                          icon: Icon(Icons.email),
-                          obscure: false,
-                          suggestions: true,
-                          onChange: (value) {
-                            email = value;
-                          }),
-                      MyFormField(
-                          inputLabel: 'Introduceți parola',
-                          icon: Icon(Icons.password),
-                          obscure: true,
-                          suggestions: false,
-                          onChange: (value) {
-                            password = value;
-                          }),
-                      MyFormField(
-                          inputLabel: "Confirmare parolă",
-                          icon: Icon(Icons.password),
-                          obscure: true,
-                          suggestions: false,
-                          onChange: (value) {
-                            confirmPass = value;
-                          }),
-                      BottomButton(
-                        label: 'Crează cont',
-                        onPress: () {
-                          print(confirmPass);
-                            // if(confirmPass != password){
-                            //   print("does not match!");
-                            print('hello');
-                            //TODO: confirm pass must match the password
-
-                        },
-                      ),
-                    ],
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: height * 0.07),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MyCircleAvatar(
+                          imageUrl: 'assets/images/BloodSugar.jpg',
+                        ),
+                        SizedBox(
+                          height: height * 0.03,
+                        ),
+                        Text(
+                          'Creare cont',
+                          style: kWelcomeText,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
+                flex: 1,
               ),
-              flex: 1,
-            ),
-          ],
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20.0),
+                        topRight: Radius.circular(20.0)),
+                    color: kFullGreen,
+                  ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        SizedBox(
+                          height: height * 0.03,
+                        ),
+                        MyFormField(
+                            inputLabel: 'Introduceți numele',
+                            icon: Icon(Icons.people),
+                            obscure: false,
+                            suggestions: true,
+                            onChange: (value) {
+                              lastName = value;
+                            }),
+                        MyFormField(
+                            inputLabel: 'Introduceți prenumele',
+                            icon: Icon(Icons.people),
+                            obscure: false,
+                            suggestions: true,
+                            onChange: (value) {
+                              firstName = value;
+                            }),
+                        MyFormField(
+                            inputLabel: 'Introduceți adresa de email',
+                            icon: Icon(Icons.email),
+                            obscure: false,
+                            suggestions: true,
+                            onChange: (value) {
+                              email = value;
+                            }),
+                        MyFormField(
+                            inputLabel: 'Introduceți parola',
+                            icon: Icon(Icons.password),
+                            obscure: true,
+                            suggestions: false,
+                            onChange: (value) {
+                              password = value;
+                            }),
+                        MyFormField(
+                            inputLabel: "Confirmare parolă",
+                            icon: Icon(Icons.password),
+                            obscure: true,
+                            suggestions: false,
+                            onChange: (value) {
+                              confirmPass = value;
+                            }),
+                        BottomButton(
+                          label: 'Crează cont',
+                          onPress: () {
+                            setState(() {
+                              showSpinner = true;
+                            });
+                            print(confirmPass);
+                              if(confirmPass != password) {
+                                print("does not match!");
+                              }
+                              print('hello');
+                              setState(() {
+                                showSpinner = false;
+                              });
+                              //TODO: confirm pass must match the password
+
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                flex: 1,
+              ),
+            ],
+          ),
         ),
       ),
     ]);
