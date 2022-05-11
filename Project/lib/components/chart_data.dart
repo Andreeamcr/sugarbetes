30fb9ab3189aeebe9f656e9628b9b17e6d0103a2
Andreea <mocriiandreea@yahoo.com>
2022-05-11 16:34:32 +0300

    chart added, needs changes

---
+++ Project/lib/components/chart_data.dart
@@ -0,0 +1,7 @@
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class ChartData {
+  ChartData(this.x, this.y);
+  final DateTime x;
+  final double? y;
+}
--- Project/lib/screens/glicemii_screen.dart
+++ Project/lib/screens/glicemii_screen.dart
@@ -4,6 +4,9 @@ import 'package:sugarbetes/screens/settings_screen.dart';
 import 'package:sugarbetes/utils/background_design.dart';
 import 'package:sugarbetes/utils/constants.dart';
 import 'package:sugarbetes/components/glucose_table.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
+import 'package:sugarbetes/components/chart_data.dart';
 
 class GlicemiiPage extends StatefulWidget {
   static String id = 'glicemii';
@@ -67,6 +70,10 @@ class _GlicemiiPageState extends State<GlicemiiPage> {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     String dbStatus = 'Deconectat';
+    final List<ChartData> chartData = [
+      ChartData(DateTime(13, 05), 68),
+      ChartData(DateTime(2020, 12, 23, 14, 15, 03), 95)
+    ];
     return Stack(
       children: [
         BackgroundColorWidget(),
@@ -79,102 +86,133 @@ class _GlicemiiPageState extends State<GlicemiiPage> {
               onPressed: () => Navigator.pop(context),
             ),
           ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                        vertical: height * 0.04, horizontal: width * 0.09),
+          extendBodyBehindAppBar: true,
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: height * 0.11,
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: width * 0.09),
+                      child: FloatingActionButton(
+                        heroTag: 'btn1',
+                        onPressed: () {
+                          setState(() {
+                            if (customIcon.icon == Icons.link_off) {
+                              customIcon = const Icon(Icons.link,
+                                  color: kFullGreen, size: 30);
+                              Navigator.pushNamed(context, SettingsPage.id);
+                            } else {
+                              customIcon = const Icon(
+                                Icons.link_off,
+                                color: kFullGreen,
+                                size: 30,
+                              );
+                            }
+                          });
+                        },
+                        child: customIcon,
+                        backgroundColor: kFullNavyBlue,
+                      ),
+                    ),
+                    Text(
+                      'Status:  $dbStatus',
+                      //TODO: de implementat variabila sa nu mai fie hardcodata
+                      style: kMathTextStyleBold,
+                    ),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.start,
+                ),
+                ListView(
+                  children: [
+                    GlucoseTable(
+                      dataRow: [
+                        DataRow(cells: [
+                          DataCell(
+                            Text(
+                              '6:48',
+                              style: kToggleText,
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              '68',
+                              style: kToggleText,
+                            ),
+                          ),
+                        ]),
+                        DataRow(cells: [
+                          DataCell(
+                            Text(
+                              '12:05',
+                              style: kToggleText,
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              '95',
+                              style: kToggleText,
+                            ),
+                          ),
+                        ]),
+                      ],
+                    ),
+                  ],
+                  shrinkWrap: true,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(width * 0.06),
+                  child: SizedBox(
+                    width: width * 0.55,
+                    height: height * 0.06,
                     child: FloatingActionButton(
-                      heroTag: 'btn1',
-                      onPressed: () {
-                        setState(() {
-                          if (customIcon.icon == Icons.link_off) {
-                            customIcon = const Icon(Icons.link,
-                                color: kFullGreen, size: 30);
-                            Navigator.pushNamed(context, SettingsPage.id);
-                          } else {
-                            customIcon = const Icon(
-                              Icons.link_off,
-                              color: kFullGreen,
-                              size: 30,
-                            );
-                          }
-                        });
+                      heroTag: 'btn2',
+                      onPressed: () async {
+                        await showInformationDialog(context);
                       },
-                      child: customIcon,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        'Adaugă o nouă valoare',
+                        style: kToggleText,
+                      ),
                       backgroundColor: kFullNavyBlue,
                     ),
                   ),
-                  Text(
-                    'Status:  $dbStatus',
-                    //TODO: de implementat variabila sa nu mai fie hardcodata
-                    style: kMathTextStyleBold,
-                  ),
-                ],
-                mainAxisAlignment: MainAxisAlignment.start,
-              ),
-              ListView(
-                children: [
-                  GlucoseTable(
-                    dataRow: [
-                      DataRow(cells: [
-                        DataCell(
-                          Text(
-                            '6:48',
-                            style: kToggleText,
-                          ),
+                ),
+                Container(
+                  child: SfCartesianChart(
+                    primaryXAxis: DateTimeAxis(
+                      minimum: DateTime.now(),
+                      intervalType: DateTimeIntervalType.hours,
+                      desiredIntervals: 1,
+                      interval: 1,
+                      maximum: DateTime.now().add(
+                        Duration(hours: 12, minutes: 59, seconds: 59),
+                      ),
+                    ),
+                    series: <CartesianSeries>[
+                      LineSeries<ChartData, DateTime>(
+                        dataSource: chartData,
+                        xValueMapper: (ChartData data, _) => data.x,
+                        yValueMapper: (ChartData data, _) => data.y,
+                        markerSettings: MarkerSettings(
+                          isVisible: true,
                         ),
-                        DataCell(
-                          Text(
-                            '68',
-                            style: kToggleText,
-                          ),
+                        dataLabelSettings: DataLabelSettings(
+                          isVisible: true,
                         ),
-                      ]),
-                      DataRow(cells: [
-                        DataCell(
-                          Text(
-                            '12:05',
-                            style: kToggleText,
-                          ),
-                        ),
-                        DataCell(
-                          Text(
-                            '95',
-                            style: kToggleText,
-                          ),
-                        ),
-                      ]),
+                      ),
                     ],
                   ),
-                ],
-                shrinkWrap: true,
-              ),
-              Padding(
-                padding: EdgeInsets.all(width * 0.06),
-                child: SizedBox(
-                  width: width * 0.55,
-                  height: height * 0.06,
-                  child: FloatingActionButton(
-                    heroTag: 'btn2',
-                    onPressed: () async {
-                      await showInformationDialog(context);
-                    },
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Text(
-                      'Adaugă o nouă valoare',
-                      style: kToggleText,
-                    ),
-                    backgroundColor: kFullNavyBlue,
-                  ),
-                ),
-              ),
-            ],
+                )
+              ],
+            ),
           ),
           // ),
           backgroundColor: Colors.transparent,
